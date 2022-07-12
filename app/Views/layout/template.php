a5b2452096d898a4db701acd3889ebb4d1d1aeae
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2022-07-12 10:58:41 +0700

    menambahkan css untuk mengatur ukuran table

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -23,6 +23,7 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url("assets/css/sb-admin-2.min.css") ?>" rel="stylesheet">
+    <link href="<?= base_url("assets/css/style.css") ?>" rel="stylesheet">
 
     <!-- Custom styles for this page -->
     <link href="<?= base_url("assets/vendor/datatables/DataTables-1.12.1/css/dataTables.bootstrap4.min.css") ?>" rel="stylesheet">
